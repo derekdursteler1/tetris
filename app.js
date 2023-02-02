83a900b5df7374816a41be620b1ffc00c26a9903
Derekdursteler <derekdursteler@gmail.com>
2023-02-01 22:47:49 -0700

    tetrimino shapes logic and start drawing

--- app.js
+++ app.js
@@ -1,7 +1,55 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartButton = document.querySelector('#start-button')
     const width = 10
 
-    console.log(squares)
+    // Tetriminoes draw shape based off google sheet pics
+    const lTetrimino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+    const zTetrimino = [
+        [width*2, width*2+1, width+1, width+2],
+        [0, width, width+1, width*2+1],
+        [width*2, width*2+1, width+1, width+2],
+        [0, width, width+1, width*2+1]
+    ]
+    const tTetrimino = [
+        [1, width, width+1, width+2],
+        [1, width+1, width+2, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1]
+    ]
+    const oTetrimino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ]
+    const iTetrimino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+    ]
+    // all shapes in array
+    const theTetriminoes = [lTetrimino, zTetrimino, tTetrimino, oTetrimino, iTetrimino]
+
+    let currentPosition = 4
+    // this grabs first shape in first position (L tetrimino)
+    let current = theTetriminoes[0][0]
+
+    //working on drawing first rotation
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetrimino')
+        })
+    }
+
+    draw()
+
 })
