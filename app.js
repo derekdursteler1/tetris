1999894bd0b7146b9525e2f472456496c1460ece
Derekdursteler <derekdursteler@gmail.com>
2023-02-03 23:11:04 -0700

    score function and deleting rows when filled

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const width = 10
     let nextRandom = 0
     let timerId
+    let score = 0
 
     // Tetriminoes draw shape based off google sheet pics
     const lTetrimino = [
@@ -100,6 +101,7 @@ document.addEventListener('DOMContentLoaded', () => {
             currentPosition = 4
             draw()
             displayShape()
+            addScore()
         }
     }
 
@@ -175,6 +177,28 @@ document.addEventListener('DOMContentLoaded', () => {
             displayShape()
         }
     })
+
+    // score
+    function addScore() {
+        for (let i = 0; i < 199; i += width) {
+            const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+
+            if(row.every(index => squares[index].classList.contains('taken'))) {
+                score += 1
+                scoreDisplay.innerHTML = score
+                row.forEach(index => {
+                    squares[index].classList.remove('taken')
+                    squares[index].classList.remove('tetrimino')
+                })
+                const squaresRemoved = squares.splice(i, width)
+                squares = squaresRemoved.concat(squares)
+                squares.forEach(cell => grid.appendChild(cell))
+            }
+        }
+    }
+
+
+
 
 
 })
